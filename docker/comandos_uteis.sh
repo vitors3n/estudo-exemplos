c4d44affaf2086a3a37c41bdf215724bcf0694d4
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-09-19 22:33:50 -0300

    docker: um comando util atualizado

--- docker/comandos_uteis.sh
+++ docker/comandos_uteis.sh
@@ -1,2 +1,5 @@
 #Lista containers com filtro
 docker container ls -f 'status=exited' -f 'status=dead' | awk '{print $4 " " $5}' | grep months
+
+#Remove imagens com status dangling
+docker rmi $(docker images -f dangling=true -q)
