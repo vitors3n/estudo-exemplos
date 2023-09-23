9425fa166581fbdfe70d6e8d082ed123b46bcb51
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-09-23 12:42:07 -0300

    linux-p2: criado script para copiar site para servidor apache

---
+++ linux/2-projeto-iac-db/iac2-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
