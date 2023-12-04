2f1e90969a8c41ad6582dc6c482903bd41b5ca4f
Vitor S <vitor.twist@gmail.com>
2023-12-04 20:20:35 -0300

    Create comandos_uteis.sh

---
+++ linux/comandos_uteis.sh
@@ -0,0 +1,7 @@
+# 1 origem
+# 2 destino
+scp -P 22 vitor@1.1.1.1:arquivo.1 vitor@2.2.2.2:arquivo.1
+# usar -r para copiar diretorios recursivamente
+
+scp -P 4422 vitor@alemanha:URL .
+#copia um arquivo do servidor alemanha para a pasta onde o comando foi rodado
