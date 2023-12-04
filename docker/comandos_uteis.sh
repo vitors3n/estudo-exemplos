b55e7b0298cc680da9f264e847d556176e553d5c
Vitor S <vitor.twist@gmail.com>
2023-12-04 18:05:13 -0300

    Update comandos_uteis.sh

--- docker/comandos_uteis.sh
+++ docker/comandos_uteis.sh
@@ -3,3 +3,6 @@ docker container ls -f 'status=exited' -f 'status=dead' | awk '{print $4 " " $5}
 
 #Remove imagens com status dangling
 docker rmi $(docker images -f dangling=true -q)
+
+#Mostra quanto de espaço em disco o docker está utilizando, images, containers etc.
+docker system df
