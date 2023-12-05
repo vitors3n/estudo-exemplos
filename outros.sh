41287b290f5f414ceba054abbd12b06b2143edb1
Vitor S <vitor.twist@gmail.com>
2023-12-05 11:26:07 -0300

    Update outros.sh

--- outros.sh
+++ outros.sh
@@ -1,8 +1,11 @@
 # Instalar IR Kernel para Jupyter
-# Como adicionar RKernel no jupyter lab instalado via PIP
-# R 'install.packages("devtools")'
-sudo apt-get install r-base # provavelmente nao será necessário.
-sudo apt install r-cran-devtools
-R -e "devtools::install_github('IRkernel/IRkernel')"
-jupyter kernelspec install /home/$USER/R/x86_64-pc-linux-gnu-library/4.1/IRkernel/kernelspec --name 'R' --user
+# Como adicionar RKernel no jupyter lab instalado via PIP no ubunto
+# R 'install.packages("devtools")' # < não usar
 
+
+# Provavelmente nao será necessário instalar o r-base.
+# Verificar versão do R antes.
+sudo apt-get install r-base && \
+sudo apt install r-cran-devtools && \
+R -e "devtools::install_github('IRkernel/IRkernel')" && \
+jupyter kernelspec install /home/$USER/R/x86_64-pc-linux-gnu-library/4.1/IRkernel/kernelspec --name 'R' --user  && \
