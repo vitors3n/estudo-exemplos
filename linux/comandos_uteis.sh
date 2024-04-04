8cd428046bdedd6eca9a51d65da4c0f880529ae1
Vitor S <vitor.twist@gmail.com>
2024-04-03 22:03:55 -0300

    Update comandos_uteis.sh

--- linux/comandos_uteis.sh
+++ linux/comandos_uteis.sh
@@ -5,3 +5,20 @@ scp -P 22 vitor@1.1.1.1:arquivo.1 vitor@2.2.2.2:arquivo.1
 
 scp -P 4422 vitor@alemanha:URL .
 #copia um arquivo do servidor alemanha para a pasta onde o comando foi rodado
+
+# ---
+
+ssh -p
+ssh -i
+
+# ---
+
+usermod -aG group user
+# a adds, G groups
+
+chmod 4760 ?
+# esp
+
+# u->user, g->group, o->others
+chmod ugo+rwx file
+
