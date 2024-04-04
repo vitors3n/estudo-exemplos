5b7edd3de140e72fdec2a92dedd19359a20333ec
Vitor S <vitor.twist@gmail.com>
2024-04-04 11:29:53 -0300

    linux: comandos úteis

--- linux/comandos_uteis.sh
+++ linux/comandos_uteis.sh
@@ -22,3 +22,15 @@ chmod 4760 ?
 # u->user, g->group, o->others
 chmod ugo+rwx file
 
+# Adicionando conteudo a arquivos
+echo "Conteudo que será adicionado ao final da linha" >> text.txt
+
+echo "Conteúdo que substituirá todo conteúdo dentro do arquivo" > text.txt
+
+#direcionando resultados de comandos para o além
+# 2>&1 -> junta 2 erro com 1 output
+mkdir > /dev/null 2>&1 #erro e resultado do comando será enviado para o além
+
+#0: standard input
+#1: standard output
+#2: standard error
