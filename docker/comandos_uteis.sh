cb2429b171ad9cbd0c23c24d929fd2c502e185b8
Vitor S <vitor.twist@gmail.com>
2024-04-23 20:55:22 -0300

    Update comandos_uteis.sh

--- docker/comandos_uteis.sh
+++ docker/comandos_uteis.sh
@@ -1,4 +1,4 @@
-#Lista containers com filtro
+#Lista containers com filtro exited ou dead
 docker container ls -f 'status=exited' -f 'status=dead' | awk '{print $4 " " $5}' | grep months
 
 #Remove imagens com status dangling
@@ -6,3 +6,12 @@ docker rmi $(docker images -f dangling=true -q)
 
 #Mostra quanto de espaço em disco o docker está utilizando, images, containers etc.
 docker system df
+
+# Update realiza o update de memoria em todos os containers
+docker update --memory 29gb --memory-swap 29gb $(docker ps | awk '{print $1}' | tail -n +2)
+
+#Joga resultado do update para o além
+docker update --memory 29gb --memory-swap 29gb $(docker ps | awk '{print $1}' | tail -n +2) > /dev/null 2>&1
+
+# Imprime lista de container IDs em execução excluindo primeira linha que possui o nome CONTAINER ID
+docker ps | awk '{print $1}' | tail -n +2
