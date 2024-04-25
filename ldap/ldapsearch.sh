4c51e4b2281daf9b5188b4faa26ff2b84b4f5063
Vitor S <vitor.twist@gmail.com>
2024-04-24 21:26:47 -0300

    Update ldapsearch.sh

--- ldap/ldapsearch.sh
+++ ldap/ldapsearch.sh
@@ -14,3 +14,12 @@ ldapsearch -x -H ldap://1.1.1.1 -b "dc=server,dc=org" "(uidNumber=*)" uidNumber 
 
 #Para remover 'uidNumber: ' do resultado é possível com o comando: sed -e
 ldapsearch -x -H ldap://1.1.1.1 -b "dc=server,dc=org" "(uidNumber=*)" uidNumber | grep uidNumber | sort -n | tail -n 1 | sed -e "s/^uidNumber: //"
+
+
+# Transforma dados da busca em apenas uma linha
+# uid=vitor uidNumber: 1001 mail: vitor@email.com
+ldapsearch -xLLL -H ldap://1.1.1.1 -b "ou=People,dc=server,dc=org" "(mail=*)" mail uidNumber | \
+        sed 's/dn: //'  | \
+        sed 's/,ou=People,dc=server,dc=org//' | \
+        sed 's/\n//' | \
+        sed ':a;N;$!ba;s/\n\n/___double_newline___/g;s/\n/ /g;s/___double_newline___/\n/g'
