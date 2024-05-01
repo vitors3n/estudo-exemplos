b55c2638f3e93316037a4c58cb418c216716f00d
Vitor S <vitor.twist@gmail.com>
2024-05-01 08:46:06 -0300

    Update comandos_uteis.sh

--- docker/comandos_uteis.sh
+++ docker/comandos_uteis.sh
@@ -8,10 +8,18 @@ docker rmi $(docker images -f dangling=true -q)
 docker system df
 
 # Update realiza o update de memoria em todos os containers
-docker update --memory 29gb --memory-swap 29gb $(docker ps | awk '{print $1}' | tail -n +2)
+docker update --memory 64gb --memory-swap 64gb $(docker ps | awk '{print $1}' | tail -n +2)
 
 #Joga resultado do update para o além
-docker update --memory 29gb --memory-swap 29gb $(docker ps | awk '{print $1}' | tail -n +2) > /dev/null 2>&1
+docker update --memory 64gb --memory-swap 64gb $(docker ps | awk '{print $1}' | tail -n +2) > /dev/null 2>&1
 
 # Imprime lista de container IDs em execução excluindo primeira linha que possui o nome CONTAINER ID
 docker ps | awk '{print $1}' | tail -n +2
+
+# Configura conteiner para sempore reiniciar
+docker update --restart unless-stopped redis
+docker update --restart unless-stopped $(docker ps -q)
+
+# on-failure[:max-retries]
+# always
+# no
