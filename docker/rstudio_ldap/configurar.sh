37be9d648d4c18633216ccd689bea990dae00a35
Vitor S. <vitor.twist@gmail.com>
2024-07-21 10:36:33 -0300

    automarizar configuracao do ldap
    Automaticamente cria os arquivos do LDAP sem necessidad de criar um por um, também adicona opcao para enviar os dados via variaveis de ambiente

---
+++ docker/rstudio_ldap/configurar.sh
@@ -0,0 +1,28 @@
+cat <<EOF > /etc/ldap.conf
+BASE $BASE
+URI $URI
+TLS_CACERT $TLS_CERT
+EOF
+
+cat <<EOF > /etc/nslcd.conf
+uri $URI
+binddn $BIND_DN
+bindpw $BIND_PW
+base $BASE
+EOF
+
+cat <<EOF > /etc/nsswitch.conf
+
+passwd:         compat ldap
+group:          compat ldap
+shadow:         compat ldap
+
+hosts:          files dns
+networks:       files
+EOF
+
+# URI: ldap://example.org
+# BASE: dc=example,dc=org
+# BIND_DN: cn=admin,dc=example,dc=org
+# BIND_PW: senha_admin
+# TLS_CERT: /etc/ssl/certs/ca-tertificates.crt
